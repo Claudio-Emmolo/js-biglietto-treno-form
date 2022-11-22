84ea772971ebac585cbeaa3d37d66ae4d1277113
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-22 15:06:34 +0100

    Creating variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const km = document.querySelector("input.kilometers");
+
+const age = document.querySelector("input.age");
+
+
+let button = document.queryCommandValue("button")
+
+
+//On click button
+button.addEventListener ("click", function (){
+	console.log (km)
+    console.log (age)
+})
