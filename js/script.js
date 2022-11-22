4a5d9038f354b85c91a9982e873c3c980be2765c
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-22 15:42:24 +0100

    Create discount and print on consol tot cost

--- js/script.js
+++ js/script.js
@@ -5,8 +5,28 @@ const age = document.getElementById("age");
 const button = document.getElementById("btn");
 
 
+let calcTicket;
+
+let discuntAge;
+
+let discountGeneral
+
+
 //On click button
 button.addEventListener ("click", function (){
-	console.log (km.value)
-    console.log (age.value)
+	console.log (parseInt(km.value));
+    console.log (parseInt(age.value));
+
+    calcTicket = km.value * 0.2762;
+
+    if (age.value < 18 ){
+        discuntAge = (calcTicket * 17.5 / 100);
+    } else if (age.value >= 65){
+        discuntAge = (calcTicket * 33.3 / 100);
+    }
+
+
+    discountGeneral = (calcTicket - discuntAge).toFixed(2);
+
+    console.log ("Il prezzo del biglietto è: " + discountGeneral);
 })
