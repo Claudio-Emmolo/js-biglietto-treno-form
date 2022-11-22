acc1caf431b4825893606ca251293bfc69094498
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-22 15:15:08 +0100

    Fix const button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-const km = document.querySelector("input.kilometers");
+const km = document.querySelector("input#kilometers");
 
-const age = document.querySelector("input.age");
+const age = document.querySelector("input#age");
 
 
-let button = document.queryCommandValue("button")
+const button = document.querySelector("button")
 
 
 //On click button
 button.addEventListener ("click", function (){
-	console.log (km)
-    console.log (age)
+	console.log (km.value)
+    console.log (age.value)
 })
